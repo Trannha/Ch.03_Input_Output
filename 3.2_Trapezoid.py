0c9a30105164b5465d0de3be51dff10763045d8e
trannha <trannha@urbandaleschools.com>
2019-09-17 14:09:13 -0500

    Finished this thing oop

--- 3.2_Trapezoid.py
+++ 3.2_Trapezoid.py
@@ -10,3 +10,9 @@ base 1: 5       base 2: 7    height: 2    area: 12
 base 1: 1       base 2: 2    height: 3    area: 4.5
 base 1: 7       base 2: 2    height: 4    area: 18
 '''
+
+base1=int(input("To solve for the are of a trapezoid, input a base value: ",))
+base2=int(input("Input a second base: "))
+height=int(input("Input the height: "))
+area=(base1+base2)/2*height
+print("The area of this trapezoid is: ", area)
